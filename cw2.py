3061ab49da2c71e9d372c4532e158521cd059f61
1234borkowskip <46341908+1234borkowskip@users.noreply.github.com>
2019-02-28 17:06:25 +0100

    Add files via upload

---
+++ cw2.py
@@ -0,0 +1,45 @@
+import math
+#zadanie 2
+print(math.ceil(5.67))
+print(math.ceil(45.34 - 6))
+print(math.floor(5.67))
+print(math.floor(22))
+print(math.floor(-4.78))
+print(math.factorial(6))
+print(math.factorial(1))
+print(math.factorial(0))
+print(math.gcd(1024,144))
+print(math.fabs(-15))
+print(math.exp(3))
+print(math.log(12))
+print(math.log10(25))
+print(math.log(6,4))
+print(math.pow(5,6))
+print(math.sqrt(34))
+print(math.pi + 6)
+print(math.cos(3))
+print(math.sin(2 * math.pi))
+#zadanie 3
+str = "programowaniestrukturalne"
+print(str)
+print(str[22:25])
+print(str[12])
+print(2 * str)
+#zadanie 4
+napis = input("Podaj napis ")
+wspak = napis[::-1]
+print(wspak)
+#zadanie 5
+napis1 = input("Podaj 1 słowo ")
+napis2 = input("Podaj 2 słowo ")
+napis3 = input("Podaj 3 słowo ")
+napis4 = input("Podaj 4 słowo ")
+napis5 = input("Podaj 5 słowo ")
+print(len(napis1))
+print(len(napis2))
+print(len(napis3))
+print(len(napis4))
+print(len(napis5))
+#zadanie 6
+tbc...
+
