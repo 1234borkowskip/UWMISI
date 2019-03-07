89af601713c8957fd925df3fcbf7d270e9e4c279
1234borkowskip <46341908+1234borkowskip@users.noreply.github.com>
2019-03-07 16:58:31 +0100

    Add files via upload

---
+++ cw3.py
@@ -0,0 +1,21 @@
+'''#zadanie 1 (caa.py)
+krotka = ("a", "B", 12, "python", 27, [2,4])
+print(krotka)
+#Nie da się zmienić elemetu w krotce
+print(krotka[3])
+print(krotka[-5])'''
+
+'''#zadanie 2 (cab.py)
+zbior = set(["Inżynieria", "Systemów", "Informatycznych"])
+print(zbior)
+zbior.add("Systemów")
+print(zbior)
+#W zbiorze nie może być więcej niż jeden dany element a w tym już istnieje element "Systemów"
+
+#Nie da się wykonać poleceń z zadania 2, ponieważ zbiory nie wspierają indeksowania elementów
+
+'''
+#zadanie 3 (cac.py)
+
+slownik = { "imie":"Paweł", "numer":984123567}
+print(slownik)
