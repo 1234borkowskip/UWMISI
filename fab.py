533348162f3a022b7271eb34306619429035ab00
1234borkowskip <46341908+1234borkowskip@users.noreply.github.com>
2019-04-04 14:08:56 +0200

    Add files via upload

---
+++ fab.py
@@ -0,0 +1,12 @@
+class osoba(object):
+    def __init__(self):
+        self.imie = "Paweł"
+        self.nazwisko = "Borkowski"
+        self.dataur = "1998-04-16"
+        self.wiek = 2019-int(self.dataur[0:4])
+    def wypisz(self):
+        print(" | ",self.imie," | ",self.nazwisko," | ",self.dataur," | ",self.wiek," | ")
+
+
+ja = osoba()
+ja.wypisz()
