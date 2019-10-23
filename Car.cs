6fe2522dfd94928ff4c98c23d981c82d184507c5
1234borkowskip <46341908+1234borkowskip@users.noreply.github.com>
2019-10-23 16:16:41 +0200

    Add files via upload

---
+++ Car.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Car
+    {
+        public string marka;
+        public string rok;
+        private string model;
+        private int iloscDrzwi;
+        private double pojemnoscSilnika;
+        public double srednieSpalanie;
+
+        private double ObliczSpalanie(double dlugoscTrasy)
+        {
+            double spalanie = srednieSpalanie * dlugoscTrasy;
+            return spalanie;
+        }
+
+        public ObliczKosztPrzejazdu( double dlugoscTrasy, double cenaPaliwa)
+        {
+            double kosztPrzejazdu = ObliczSpalanie(dlugoscTrasy) * cenaPaliwa;
+            return kosztPrzejazdu;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {      
+            /*string carName = "Mój Samochód";
+            Console.WriteLine(carName);
+            Car car1 = new Car();
+            car1.marka = "BMW";
+            car1.rok = "1999";
+            Console.WriteLine(car1.marka + " " + car1.rok);
+            Console.ReadKey();
+            Car car2 = new Car();
+            car2.marka = "Opel";
+            car2.rok = "1997";
+            Console.WriteLine(car2.marka + " " + car2.rok);
+            Console.ReadKey();
+            car1 = car2;
+            Console.WriteLine(car1.marka + " " + car1.rok);
+            Console.ReadKey();*/
+
+        }
+    }
+}
