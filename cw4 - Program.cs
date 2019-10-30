9117795c2befc06e216c0e20abff928c00b496ee
1234borkowskip <46341908+1234borkowskip@users.noreply.github.com>
2019-10-30 15:59:16 +0100

    Add files via upload

---
+++ cw4 - Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cw4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Osoba ja = new Osoba("x", "DDDDDD", 1999);
+            ja.Wypiszinfo();
+            Student ja2 = new Student("x", "DDDD", 1999, 2, 1, 151280);
+            ja2.Wypiszinfo();
+            Console.ReadKey();
+
+        }
+    }
+}
