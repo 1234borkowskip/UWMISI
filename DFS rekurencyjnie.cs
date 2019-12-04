e95d0a78f484974f289814a0cbf8e931ab6eb690
1234borkowskip <46341908+1234borkowskip@users.noreply.github.com>
2019-12-04 18:05:11 +0100

    Add files via upload

---
+++ DFS rekurencyjnie.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        class Graf
+        {
+            List<Node> odwiedzone;
+        }
+        public void PrzejscieDFS()
+        {
+            this.odwiedzone = new List<Node>();
+            this.DFS(start);
+            //wypisać liste odwiedzonych
+        }
+        public void DFS(Node n)
+        {
+            if (this.odwiedzone.Contains(n)) //sprawdzam czy odwiedzony
+                return;
+            this.odwiedzone.Add(n);
+            var krawedzieOd = this.ZnajdzKrawedzie(n);   // wszystkie krawedzie z node n
+            foreach(var k in krawedzieOd)
+            {
+                var drugi = k.WezDrugi(n); //drugi wierzcholek
+                DFS(drugi); // powtórz
+            }
+        }
+
+
+    }
+}
